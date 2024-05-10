ce9bb3d8df5f9aaa0ed76230f9d7693763ea5eac
Olivia Darby <oliviadarby@live.co.uk>
2024-05-10 11:20:10 +0100

    logo page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import React from "react";
 function App() {
   return (
     <>
-      <div className="uppercase tracking-wide">Portfolio</div>
+      <div className="flex justify-center items-center h-screen bg-[#2a2a2a]">
+        <img src="../assets/liv.png" alt="Logo" className="w-[30%]" />
+      </div>
     </>
   );
 }
